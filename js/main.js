406859947132b3096fc3ebc4cd1b591e49d0aecb
fv_game <fv_game@mail.ru>
2018-11-22 02:35:27 +0300

    Переключение экранов кнопками стрелок на клавиатуре.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,34 @@
 
 const main = document.querySelector(`#main`);
 const templates = document.querySelectorAll(`template`);
-
+let currentTemplateIndex = -1;
 const setTemplate = (index) => {
   const getTemplate = templates[index] || false;
   if (getTemplate) {
     main.innerHTML = ``;
     main.appendChild(document.importNode(getTemplate.content, true));
   }
+  currentTemplateIndex = index;
+};
+setTemplate(0);
+
+const goPrev = () => {
+  if (currentTemplateIndex > 0) {
+    setTemplate(currentTemplateIndex - 1);
+  }
+};
+const goNext = () => {
+  if (currentTemplateIndex < templates.length) {
+    setTemplate(currentTemplateIndex + 1);
+  }
 };
 
-setTemplate(0);
+document.addEventListener(`keydown`, (event) => {
+  switch (event.which) {
+    case 37:goPrev();
+      break;
+    case 39:goNext();
+      break;
+  }
+  console.log(currentTemplateIndex);
+});
