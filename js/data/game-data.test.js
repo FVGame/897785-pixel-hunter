f792c6920955bae9e3b16f4972d389121e10e0a9
fv_game@mail.ru <fv_game@mail.ru>
2018-12-20 13:03:34 +0300

    модуль подсчёта статистики, добавлены тесты для быстрых и медленных ответов.

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -21,6 +21,48 @@ describe(`stats calculator`, () => {
     assert.equal(statsCalc([], 0), -1);
   });
   it(`should allow 10 answers`, () => {
-    assert.equal(statsCalc(Array(10), 3), 0);
+    const answers = [
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+      {answer: 1},
+    ];
+    assert.equal(statsCalc(answers, 3), 1000);
+  });
+  it(`should allow fast 10 answers`, () => {
+    const answers = [
+      {time: 0},
+      {time: 0},
+      {time: 0},
+      {time: 0},
+      {time: 0},
+      {time: 0},
+      {time: 0},
+      {time: 0},
+      {time: 0},
+      {time: 0},
+    ];
+    assert.equal(statsCalc(answers, 3), 1500);
+  });
+  it(`should allow slow 10 answers`, () => {
+    const answers = [
+      {time: 999},
+      {time: 999},
+      {time: 999},
+      {time: 999},
+      {time: 999},
+      {time: 999},
+      {time: 999},
+      {time: 999},
+      {time: 999},
+      {time: 999},
+    ];
+    assert.equal(statsCalc(answers, 3), 500);
   });
 });
--- js/stats-calc.js
+++ js/stats-calc.js
@@ -1,7 +1,22 @@
 const calc = (answers, leaves) => {
-  let points = -1;
+  let points = 0;
   if (answers && typeof answers === `object` && answers.length === 10 && leaves && typeof leaves === `number`) {
-    points = 0;
+    const checkAnswer = () => {
+      return true;
+    };
+    answers.forEach((answer) => {
+      if (!checkAnswer(answer.answer)) {
+        return;
+      }
+      points += 100;
+      if (answer.time < 5) {
+        points += 50;
+      } else if (answer.time > 30) {
+        points -= 50;
+      }
+    });
+  } else {
+    points = -1;
   }
   return points;
 };
