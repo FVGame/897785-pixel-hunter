697f6b2d0f4a55dd0429794f1fb78dc191ea5599
fv_game@mail.ru <fv_game@mail.ru>
2018-12-21 18:07:53 +0300

    промежуточный.

---
+++ js/data-game1.js
@@ -0,0 +1,13 @@
+export default {
+  taskDescription: `Угадайте для каждого изображения фото или рисунок?`,
+  paintings: [
+    `https://k42.kn3.net/CF42609C8.jpg`, // People
+    `https://k42.kn3.net/D2F0370D6.jpg`, // Animals
+    `https://k32.kn3.net/5C7060EC5.jpg` // Nature
+  ],
+  photos: [
+    `http://i.imgur.com/1KegWPz.jpg`, // People
+    `https://i.imgur.com/DiHM5Zb.jpg`, // Animals
+    `http://i.imgur.com/DKR1HtB.jpg` // Nature
+  ]
+};
--- js/template-game-1.js
+++ js/template-game-1.js
@@ -1,8 +1,9 @@
 import getElementFromHtml from "./get-element-from-html";
 import templateGame2 from './template-game-2';
 import setTemplate from './set-template';
+import data from './data-game1';
 
-const game1 = getElementFromHtml(`<div><header class="header">
+const htmlHeader = `<header class="header">
     <button class="back">
       <span class="visually-hidden">Вернуться к началу</span>
       <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
@@ -18,34 +19,26 @@ const game1 = getElementFromHtml(`<div><header class="header">
       <img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">
       <img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">
     </div>
-  </header>
-  <section class="game">
-    <p class="game__task">Угадайте для каждого изображения фото или рисунок?</p>
-    <form class="game__content">
-      <div class="game__option">
-        <img src="http://placehold.it/468x458" alt="Option 1" width="468" height="458">
+  </header>`;
+const createForm = (itemsLength = 1) => {
+  let html = `<form class="game__content ${itemsLength === 3 ?}">`;
+
+    ${createGameOption(data.paintings[0], 1, 468, 458)}
+    ${createGameOption(data.photos[0], 2, 468, 458)}
+    </form>
+};
+const createGameOption = (src, index, width, height) => `<div class="game__option">
+        <img src="${src}" alt="Option ${index}" width="${width}" height="${height}">
         <label class="game__answer game__answer--photo">
-          <input class="visually-hidden" name="question1" type="radio" value="photo">
+          <input class="visually-hidden" name="question${index}" type="radio" value="photo">
           <span>Фото</span>
         </label>
         <label class="game__answer game__answer--paint">
-          <input class="visually-hidden" name="question1" type="radio" value="paint">
+          <input class="visually-hidden" name="question${index}" type="radio" value="paint">
           <span>Рисунок</span>
         </label>
-      </div>
-      <div class="game__option">
-        <img src="http://placehold.it/468x458" alt="Option 2" width="468" height="458">
-        <label class="game__answer  game__answer--photo">
-          <input class="visually-hidden" name="question2" type="radio" value="photo">
-          <span>Фото</span>
-        </label>
-        <label class="game__answer  game__answer--paint">
-          <input class="visually-hidden" name="question2" type="radio" value="paint">
-          <span>Рисунок</span>
-        </label>
-      </div>
-    </form>
-    <ul class="stats">
+      </div>`;
+const htmlStats = `<ul class="stats">
       <li class="stats__result stats__result--wrong"></li>
       <li class="stats__result stats__result--slow"></li>
       <li class="stats__result stats__result--fast"></li>
@@ -56,7 +49,13 @@ const game1 = getElementFromHtml(`<div><header class="header">
       <li class="stats__result stats__result--unknown"></li>
       <li class="stats__result stats__result--unknown"></li>
       <li class="stats__result stats__result--unknown"></li>
-    </ul>
+    </ul>`;
+// game__content--triple
+const game1 = getElementFromHtml(`<div>${htmlHeader}
+  <section class="game">
+    <p class="game__task">${data.taskDescription}</p>
+    
+    ${htmlStats}
   </section></div>`);
 const groupsCount = 2;
 game1.querySelector(`.game__content`).addEventListener(`click`, (event) => {
