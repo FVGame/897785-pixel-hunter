387372c2a85e8a9ec3e014a15dd3370ae969a1d1
fv_game <fv_game@mail.ru>
2018-12-24 20:13:06 +0300

    новые фото и картинки прописаны в data-game1 модуле.

--- js/data-game1.js
+++ js/data-game1.js
@@ -3,11 +3,68 @@ export default {
   paintings: [
     `https://k42.kn3.net/CF42609C8.jpg`, // People
     `https://k42.kn3.net/D2F0370D6.jpg`, // Animals
-    `https://k32.kn3.net/5C7060EC5.jpg` // Nature
+    `https://k32.kn3.net/5C7060EC5.jpg`, // Nature
+    `/img/paints/010.jpg`,
+    `/img/paints/011.jpg`,
+    `/img/paints/012.jpg`,
+    `/img/paints/013.jpg`,
+    `/img/paints/014.jpg`,
+    `/img/paints/015.jpg`,
+    `/img/paints/016.jpg`,
+    `/img/paints/017.jpg`,
+    `/img/paints/018.jpg`,
+    `/img/paints/377835.jpg`,
+    `/img/paints/377836.jpg`,
+    `/img/paints/377847.jpg`,
+    `/img/paints/b-IMG_5b6e0ed7247c.jpg`,
+    `/img/paints/b-IMG_6b747c554688.jpg`,
+    `/img/paints/b-IMG_6cd6fbdfe406.jpg`,
+    `/img/paints/b-IMG_7d7f65b2ec70.jpg`,
+    `/img/paints/b-IMG_7d77697ae4ae.jpg`,
+    `/img/paints/b-IMG_8b00b4192db5.jpg`,
+    `/img/paints/b-IMG_45f189624bc5.jpg`,
+    `/img/paints/b-IMG_67d6548d2d46.jpg`,
+    `/img/paints/b-IMG_79d9f0f8c679.jpg`,
+    `/img/paints/b-IMG_415b33db10b2.jpg`,
+    `/img/paints/b-IMG_522bb9ed3e14.jpg`,
+    `/img/paints/b-IMG_1755cd2b2e16.jpg`,
+    `/img/paints/b-IMG_7148cfb5cade.jpg`,
+    `/img/paints/b-IMG_26193ea99c68.jpg`,
+    `/img/paints/b-IMG_a06c287a33ba.jpg`,
+    `/img/paints/b-IMG_a348769741db.jpg`,
+    `/img/paints/b-IMG_b5b6f86eb13b.jpg`,
+    `/img/paints/b-IMG_b58eff97cc94.jpg`,
+    `/img/paints/b-IMG_dfc10892a9dd.jpg`,
+    `/img/paints/b-IMG_e3337ae927c0.jpg`,
+    `/img/paints/b-IMG_ffdace5dbd39.jpg`,
+    `/img/paints/IMG_6fd1efe17ce9.jpg`,
+    `/img/paints/IMG_8b440da68809.jpg`,
+    `/img/paints/IMG_50c5e237f704.jpg`,
+    `/img/paints/IMG_86b805c6b84e.jpg`,
+    `/img/paints/IMG_37633c8246c2.jpg`,
+    `/img/paints/IMG_82531029870c.jpg`,
+    `/img/paints/IMG_a9e94598ff4c.jpg`,
+    `/img/paints/tn.jpg`,
   ],
   photos: [
     `http://i.imgur.com/1KegWPz.jpg`, // People
     `https://i.imgur.com/DiHM5Zb.jpg`, // Animals
-    `http://i.imgur.com/DKR1HtB.jpg` // Nature
+    `http://i.imgur.com/DKR1HtB.jpg`, // Nature
+    `/img/photos/1QyvYbd.jpg`,
+    `/img/photos/2vJM1W9.jpg`,
+    `/img/photos/5na6c1v.jpg`,
+    `/img/photos/6Pnz354.jpg`,
+    `/img/photos/8lxA7yg.jpg`,
+    `/img/photos/f5EZkQ5.jpg`,
+    `/img/photos/I4YSJhN.jpg`,
+    `/img/photos/NML6j3H.jpg`,
+    `/img/photos/O0ZGFf6.jpg`,
+    `/img/photos/veledzimovich_1.jpg`,
+    `/img/photos/veledzimovich_2.jpg`,
+    `/img/photos/veledzimovich_3.jpg`,
+    `/img/photos/veledzimovich_4.jpg`,
+    `/img/photos/veledzimovich_5.jpg`,
+    `/img/photos/veledzimovich_8.jpg`,
+    `/img/photos/YPwRZWv.jpg`
   ]
 };
