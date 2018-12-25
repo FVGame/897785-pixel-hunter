c2c628990208a18b3a6d4c3e1f2c0842ed875085
fv_game@mail.ru <fv_game@mail.ru>
2018-12-25 13:49:48 +0300

    html статистики и шапка игры вынесены в отдельные модули.

--- js/template-game-1.js
+++ js/template-game-1.js
@@ -3,6 +3,8 @@ import templateGame1 from './template-game-1';
 import setTemplate from './set-template';
 import data from './data-game1';
 import randomInt from './random-integer';
+import gameHeader from './game-header';
+import gameStats from './game-stats';
 
 let itemsLength = randomInt(1, 4);
 const items = {
@@ -10,23 +12,6 @@ const items = {
   TWO: 2,
   THREE: 3
 };
-const htmlHeader = `<header class="header">
-    <button class="back">
-      <span class="visually-hidden">Вернуться к началу</span>
-      <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
-        <use xlink:href="img/sprite.svg#arrow-left"></use>
-      </svg>
-      <svg class="icon" width="101" height="44" viewBox="0 0 101 44" fill="#000000">
-        <use xlink:href="img/sprite.svg#logo-small"></use>
-      </svg>
-    </button>
-    <div class="game__timer">NN</div>
-    <div class="game__lives">
-      <img src="img/heart__empty.svg" class="game__heart" alt=" Missed Life" width="31" height="27">
-      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">
-      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">
-    </div>
-  </header>`;
 const createForm = () => {
   let additionalClasses = ``;
   let width = 0;
@@ -71,22 +56,10 @@ const addLabels = (index) => `<label class="game__answer game__answer--photo">
           <input class="visually-hidden" name="question${index}" type="radio" value="paint">
           <span>Рисунок</span>
         </label>`;
-const htmlStats = `<ul class="stats">
-      <li class="stats__result stats__result--wrong"></li>
-      <li class="stats__result stats__result--slow"></li>
-      <li class="stats__result stats__result--fast"></li>
-      <li class="stats__result stats__result--correct"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-    </ul>`;
-const game1 = getElementFromHtml(`<div>${htmlHeader}
+const game1 = getElementFromHtml(`<div>${gameHeader}
   <section class="game">
     ${createForm()}
-    ${htmlStats}
+    ${gameStats}
   </section></div>`);
 const addEventsGame1 = () => {
   game1.querySelectorAll(`.game__answer input`).forEach((input) => {
